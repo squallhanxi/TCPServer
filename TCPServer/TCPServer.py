012985b2ae8475716673a7c574b0c5ca88bf8c0b
Squall Hon <hxbyte@163.com>
2018-10-19 22:21:11 +0800

    Initial commit

---
+++ TCPServer/TCPServer.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+# !/usr/bin/env python
+ 
+from socket import *
+from time import ctime
+import threading
+import time
+ 
+HOST = ''
+PORT = 8080
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+ 
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+socks = []  # 放每个客户端的socket
+ 
+ 
+def handle():
+    while True:
+        for s in socks:
+            try:
+                data = s.recv(BUFSIZ)  # 到这里程序继续向下执行
+            except:
+                continue
+            if not data:
+                print('断开连接:',s.getpeername())
+                socks.remove(s)
+                continue
+            for ss in socks:
+                ss.send(data)
+  
+t = threading.Thread(target=handle)  # 子线程
+
+if __name__ == '__main__':
+    t.start()
+    print('等待连接...')
+    while True:
+        clientSock, addr = tcpSerSock.accept()
+        print('连接来自:', addr)
+        clientSock.setblocking(0)
+        socks.append(clientSock)
+ 
+ 
